a78ae95a8d2554eecad04f626850216f41ce1127
Gabriel Espindola <gabrielsaimo68@gmail.com>
2026-01-28 23:40:27 -0300

    v11.1

--- src/components/AppDownload.tsx
+++ src/components/AppDownload.tsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import './AppDownload.css';
 
-const APK_DOWNLOAD_URL = 'https://github.com/gabrielsaimo/free-tv/releases/download/v11/saimo_tv_v11.apk';
-const APP_VERSION = '11';
+const APK_DOWNLOAD_URL = 'https://github.com/gabrielsaimo/free-tv/releases/download/V11.1/Saimo-TV-v11.1.apk';
+const APP_VERSION = '11.1';
 
 export function AppDownload() {
   const [isLoaded, setIsLoaded] = useState(false);
